0d86a763d5fc16cc4da651b622d05fe5c715fd2b
Ankita Yadav <ankitacoder2004@gmail.com>
2020-09-18 23:39:27 +0530

    triplet done

---
+++ triplet.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr1[] = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        int arr2[] = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        int scoreA = 0;
+        int scoreB = 0;
+        for (int i = 0; i < 3; i++) {
+            if (arr1[i] > arr2[i]) {
+                scoreA++;
+
+            } else if (arr2[i] > arr1[i]) {
+                scoreB++;
+            }
+
+        }
+        System.out.println(scoreA + " " + scoreB);
+        sc.close();
+    }
+}
